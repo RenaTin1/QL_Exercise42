0fe38acfaa18f636905a679b79f1fee5e963edfc
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-19 21:35:07 -0400

    Exercise 42

---
+++ Exercise42/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42
+{
+    class Point
+    {
+        //class members
+        private double x;
+        private double y;
+
+        //properties
+        public double X
+        {
+            set { x = value; }
+            get { return x; }
+        }
+        public double Y
+        {
+            set { y = value; }
+            get { return y; }
+        }
+
+        //constructors
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(double Xx, double Yy)
+        {
+            X = Xx;
+            Y = Yy;
+        }
+
+
+        public double calcDistance()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+
+        }
+
+
+    }
+}
---
+++ Exercise42/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> AllPoints = new List<Point>();
+
+            Console.WriteLine("Please enter an X Coordinate:");
+            double Xx = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter an Y Coordinate:");
+            double Yy = double.Parse(Console.ReadLine());
+
+            Point MyPoints = new Point(Xx, Yy);
+
+            Console.WriteLine($"You have created a point object({Xx},{Yy})");
+            Console.WriteLine(MyPoints.calcDistance());
+
+        }
+
+    }
+}
+
